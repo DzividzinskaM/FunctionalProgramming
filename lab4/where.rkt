17c9d03707d530a82ea6a959082e94d30c5d56d8
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-04-08 13:43:05 +0300

    add where

--- lab4/where.rkt
+++ lab4/where.rkt
@@ -19,30 +19,50 @@
     (#t (display "Column don't find"))))
 
 
+
 (define (returnToTableByColumn table)
   (hash-clear! table)
   (map (lambda (l)
          (hash-set! table (first l) l))
        ListRows))
-               
+
+
+(define (GetColumnAndValue conditionLine separator)
+  (set! column (first (string-split conditionLine separator)))
+  (CheckColumn)
+  (set! value (second (string-split conditionLine separator)))
+  (set! index (index-of (first ListRows) column)))
+
+
+
+(define (ConditionMoreEqual condition table)
+  (GetColumnAndValue condition ">=")
+  (cond
+    ((not (string->number value)) (println "Value must to be number"))
+    (#t (set! value (string->number value))))
+  (set! ListRows (cddr ListRows))
+  (map (lambda (l)
+         (cond
+           ((not (>= (string->number (list-ref l index)) value)) (set! ListRows (remove l ListRows)))
+           )) 
+        ListRows)
+  (set! ListRows (append (list (reverse header)) ListRows)))
+
+
 
 (define (ConditionEqual condition table)
-  (set! column (first (string-split condition "=")))
-  (CheckColumn)
-  (set! value (second (string-split condition "=")))
-  (set! index (index-of (first ListRows) column))
+  (GetColumnAndValue condition "=")
   (map (lambda (l)
          (cond
            ((equal? (list-ref l index) value) #t)
            (#t (set! ListRows (remove l ListRows)))))
        ListRows)
-  (set! ListRows (append (list (reverse header)) ListRows))
-  (display ListRows)
-  (display "\n"))
+  (set! ListRows (append (list (reverse header)) ListRows)))
   
 
 (define (getCondition condition table)
   (cond
+    ((string-contains? condition ">=") (ConditionMoreEqual condition table))
     ((string-contains? condition "=") (ConditionEqual condition table))
     (#t (display "Operator don't find\n"))))
 
@@ -51,6 +71,7 @@
          
 
 (define (where condition table)
+  (set! ListRows null)
   (set! ListRows (append ListRows (hash->list table)))
   (set! ListRows (transpose ListRows))
   (set! header (hash-keys table)) 
